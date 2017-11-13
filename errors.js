d325ec82263237dffcf74df24625ad66c67251ab
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-11-13 16:18:06 +1000

    fix: remove ut-error

--- errors.js
+++
@@ -1,7 +0,0 @@
-let create = require('ut-error').define;
-let Queue = create('queue');
-
-module.exports = {
-    echoTimeout: create('echoTimeout', Queue, 'Timed out waiting for incoming data'),
-    invalidRead: create('invalidRead', Queue, 'Unexpected read from queue')
-};
